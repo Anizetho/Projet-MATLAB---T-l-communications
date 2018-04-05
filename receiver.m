77d7e05ca3a891a52b7b9236c0875d021ebc3696
Alex Notes <me@axn.io>
2018-04-06 00:07:21 +0200

    saleté de filtre qui marche po

---
+++ receiver.m
@@ -0,0 +1,12 @@
+% Paramètre receiver
+% aucun...
+
+sHighFFT = fft(data);
+
+for n = 1:N
+    if n == 1
+        z(:,n) = sHighFFT(1:1/Tb);
+    else
+        z(:,n) = sHighFFT(carfreq(n)-1/Tb+1:carfreq(n)+1/Tb);
+    end
+end
--- sender.m
+++ sender.m
@@ -1,14 +1,13 @@
 % Paramètres émetteur
-M = 10;     % nombre de bits
-R = 10;     % débit binaire bits/s
-Tb = 1/R;   % durée d'un bit
-roll = 0;   % facteur de 'rolloff'
-L = 2;      % largeur de bande xTb
-beta = 4*N; % suréchantillonage (donnée dans l'énoncé)
+R = 10;         % débit binaire bits/s
+Tb = 1/R;       % durée d'un bit
+roll = 0;       % facteur de 'rolloff'
+L = 1;          % largeur de bande xTb
+beta = 4*N;     % suréchantillonage (donnée dans l'énoncé)
 Tn = 1/beta*Tb; % nouvelle fréquence d'échantillonage
 fir = rcosdesign(roll, 2, beta);
 
-carfreq = (0:N-1)'*L/Tb; % fréquences des porteuses
+carfreq = (0:N-1)'*2*L/Tb; % fréquences des porteuses
 carrier = cos(carfreq*linspace(0, 2*pi, 1/Tn))';
 
 % fonction qui map 0,1 en -1,1
@@ -22,15 +21,18 @@ s = filter(fir, 1, al);
 
 sHigh = s .* carrier;
 sHighFFT = fft(sHigh);
-sHighFFTfiltered = sHighFFT;
+sHighFFT = sHighFFT(1:end/2+1,:); % one-sided spectrum
+sHighFFTb = sHighFFT;
 
-for x = 1:N
-    sHighFFTfiltered(carfreq(x)+1/Tb+1:end/2, x) = 0;
-    sHighFFTfiltered(1:carfreq(x)-1/Tb, x) = 0;
-    
-    sHighFFTfiltered(end-carfreq(x)+1/Tb+2:end, x) = 0;
-    sHighFFTfiltered(end/2: end-carfreq(x)-1/Tb+1, x) = 0;
+for n = 1:N
+    % filtre passe-bande "à la brute"
+    sHighFFTb(carfreq(n)+L*1/Tb+1:end, n) = 0;
+    sHighFFTb(1:carfreq(n)-L*1/Tb, n) = 0;
 end
+
+% reconstruction des spectres
+sHighFFT  = vertcat(sHighFFT , flip(sHighFFT (2:end-1,:)));
+sHighFFTb = vertcat(sHighFFTb, flip(sHighFFTb(2:end-1,:)));
 
 subplot(2,2,1)
 stem(sHigh)
@@ -39,8 +41,11 @@ subplot(2,2,3)
 stem(abs(sHighFFT)/(M*beta))
 grid
 subplot(2,2,2)
-stem(ifft(sHighFFTfiltered))
+sHighb = ifft(real(sHighFFTb));
+stem(sHighb)
 grid
 subplot(2,2,4)
-stem(abs(sHighFFTfiltered)/(M*beta))
+stem(abs(sHighFFTb)/(M*beta))
 grid
+
+data = sum(sHighb, 2);
