cb751adb3e2fb363983e0405bde4c374d2b3a2d6
Alex Notes <me@axn.io>
2018-04-09 20:50:24 +0200

    prend en compte le rolloff dans le spectre

--- sender.m
+++ sender.m
@@ -7,7 +7,7 @@
 % Sender parameters
 R = 10;         % bit rate
 Tb = 1/R;       % bit duration
-roll = 0;       % rolloff factor
+roll = 0.25;    % rolloff factor
 L = 1;          % bandwidth xTb
 beta = 4*N;     % upsampling factor
 Tn = Tb/beta;   % upsample sampling rate
@@ -24,7 +24,7 @@ s = upfirdn(a, fir, beta);
 % transmission length
 len = size(s, 1);
 
-carfreq = (0:N-1)'*2*L/Tb; % carriers frequencies
+carfreq = (0:N-1)'*2*L*(1+roll)/Tb; % carriers frequencies
 carrier = cos(carfreq/(len*Tn)*linspace(0, 2*pi*len*Tn, len))';
 
 sHigh = s .* carrier;
@@ -36,9 +36,11 @@ if M ~= 1
     % cut the one-sided spectrum
     sHighFFTb = sHighFFT(1:ceil(end/2)+1,:);
     for n = 1:N
-        % bandpass filter
-        sHighFFTb(1:carfreq(n)-L*1/Tb,n) = 0;
-        sHighFFTb(carfreq(n)+L*1/Tb+1:end,n) = 0;
+        % bandpass filter:
+        % - before the bandwidth
+        sHighFFTb(1:carfreq(n)-L*ceil((1+roll)/Tb),n) = 0;
+        % - after the bandwidth
+        sHighFFTb(carfreq(n)+L*floor((1+roll)/Tb)+1:end,n) = 0;
     end
     % recreate the two-sided sprectum
     sHighFFTb = vertcat(sHighFFTb, conj(flipud(sHighFFTb(2:end-2,:))));
