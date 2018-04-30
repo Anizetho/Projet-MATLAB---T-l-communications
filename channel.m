f37cabf93defb3f97eb84d95eabd97b62c72692c
Alex Notes <me@axn.io>
2018-04-30 19:13:51 +0200

    enable canal

--- channel.m
+++ channel.m
@@ -4,16 +4,16 @@
 % Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 
 % AWGN: independant samples, null mean and unitary variance
-noise = randn([numel(data)+shift 1]);
+noise = 0.2*randn([numel(data)+shift 1]);
 % damping factor on each channel, it has to be <1
-alpha = noise(1, 1);
+alpha = 0.8;
 
-noise_filter = design(fdesign.lowpass('N,F3db', 30, 2*N/Tb, 2/Tn));
+noise_filter = design(fdesign.lowpass('N,F3db', 30, 2*N*L/Tb, 2/Tn));
 noise_f = filter(noise_filter, noise);
 
 variance = 0.1;
 std_dev = sqrt(variance);
 noise_f = noise_f*std_dev;
 
-data = [zeros(shift,1); data];
 data = alpha*data+noise_f;
+data = [zeros(shift,1); data];
