4c62e0244d784f474436bacea09b09ca7200e3c0
Alex Notes <me@axn.io>
2018-05-03 12:09:57 +0200

    Ajout de l'axe temporel

--- main.m
+++ main.m
@@ -15,14 +15,15 @@ receiver
 % compare the sent signal with the received one
 figure
 subplot(2,1,1)
-stem(s1(:,2));
+stem(linspace(0, len1*Tn, len1), s1(:,2));
 title('Signal normalisé envoyé par l''émeteur')
 xlabel('Temps de transmission (s)')
 ylabel('Amplitude du signal')
 grid
 
 subplot(2,1,2)
-stem(s2(:,2), 'Color', [0.85 0.33 0.1]);
+len3 = size(s2,1);
+stem(linspace(0, len3*Tn, len3), s2(:,2), 'Color', [0.85 0.33 0.1]);
 title('Signal recomposé dans le receveur')
 xlabel('Temps de transmission (s)')
 ylabel('Amplitude du signal')
