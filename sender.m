063701d23bb92ad38eb7bc33898b2de36dd09355
Alex Notes <me@axn.io>
2018-06-12 21:24:00 +0200

    Décommente les figures dans les .m

--- sender.m
+++ sender.m
@@ -29,30 +29,30 @@ s1High = s1High.*sqrt(ratio);
 data = sum(s1High, 2);
 
 % plot impulsions
-% iX = linspace(0, span/1e2, 1e2*span+1);
-% iY = rcosdesign(roll, span, 1e2);
-% plot(iX, iY' * ones(1, N) .* ...
-%      cos(carfreq*linspace(0, 2*pi, span*1e2+1))')
-% ylim([-max(iY)*1.1 +max(iY)*1.1])
-% title("Representation temporelle des impulsions utilisees")
-% ylabel("Coefficient d'amplitude"), xlabel("Temps (s)")
-% legend(strcat("Canal ", num2str((1:N)')))
-% grid
-% clear iX iY
+iX = linspace(0, span/1e2, 1e2*span+1);
+iY = rcosdesign(roll, span, 1e2);
+plot(iX, iY' * ones(1, N) .* ...
+     cos(carfreq*linspace(0, 2*pi, span*1e2+1))')
+ylim([-max(iY)*1.1 +max(iY)*1.1])
+title("Representation temporelle des impulsions utilisees")
+ylabel("Coefficient d'amplitude"), xlabel("Temps (s)")
+legend(strcat("Canal ", num2str((1:N)')))
+grid
+clear iX iY
 
 % plot visual representation of the transmission
-% figure
-% subplot(2,1,1)
-% stem(linspace(0, len1*Tn, len1), s1High)
-% title('Representation temporelle du signal envoye')
-% ylabel('Amplitude (v)'), xlabel('Times (s)')
-% legend(strcat("Canal ", num2str((1:N)')), 'Location', 'NorthEast')
-% grid
+figure
+subplot(2,1,1)
+stem(linspace(0, len1*Tn, len1), s1High)
+title('Representation temporelle du signal envoye')
+ylabel('Amplitude (v)'), xlabel('Times (s)')
+legend(strcat("Canal ", num2str((1:N)')), 'Location', 'NorthEast')
+grid
 
-% subplot(2,1,2)
-% semilogy(linspace(0, 1/Tn-1, len1), abs(fft(s1High/len1)).^2)
-% ylim([10^-6 10^0])
-% title('Representation frequentielle du signal envoye')
-% ylabel('Puissance (dBm)'), xlabel('Frequency (Hz)')
-% legend(strcat("Canal ", num2str((1:N)')), 'Location', 'North')
-% grid
+subplot(2,1,2)
+semilogy(linspace(0, 1/Tn-1, len1), abs(fft(s1High/len1)).^2)
+ylim([10^-6 10^0])
+title('Representation frequentielle du signal envoye')
+ylabel('Puissance (dBm)'), xlabel('Frequency (Hz)')
+legend(strcat("Canal ", num2str((1:N)')), 'Location', 'North')
+grid
